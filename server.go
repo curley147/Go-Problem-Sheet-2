d5a695d040e4f01187a51df31604e1cab8d5b623
Micheal Curley <G00340243@gmit.ie>
2017-10-26 22:16:08 +0100

    Q5 commit using template to display message

--- server.go
+++ server.go
@@ -1,14 +1,25 @@
 package main
 
 import (
-  "net/http"
+	"net/http"
+	"text/template"
+	"fmt"
 )
+
+type Messagedata struct{
+	Message string
+}
+
+func guessHandler(w http.ResponseWriter, r *http.Request){
+	t, err := template.ParseFiles("template/guess.tmpl")
+	if err != nil {
+		fmt.Println("template retrieval failed:", err)
+	}
+	t.Execute(w, Messagedata{Message: "Guess a number between 1 and 20"})
+}
 
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("./Ps2")))
+	http.HandleFunc("/guess", guessHandler)
 	http.ListenAndServe(":8080", nil)
 }
-
-// func HomePage(w http.ResponseWriter, r *http.Request){
-    
-// }
